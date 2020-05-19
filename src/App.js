e6d4c74edc1ac3301e97d3db97e75c293c6331b6
Enrique Tellez <enriquetellez@MacBook-Pro.local>
2020-05-18 22:48:35 -0400

    Added login component layout and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Iphone from './components/iphone/Iphone'
 import Macbook from './components/macbook/Macbook'
 import Watch from './components/watch/Watch'
 import Register from './components/user/Register'
+import Login from './components/user/Login'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/macbook" component={Macbook} />
         <Route exact path="/watch" component={Watch} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </Router>
   )
---
+++ src/components/user/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Card, CardContent, TextField, Typography, Button } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  gridStyle: {
+    height: '100vh',
+    width: '100%',
+    background: 'linear-gradient(180deg, #5AC8FA 25%, #FFFFFF 70%)'
+  },
+  signup: {
+    marginTop: '30px',
+    marginBottom: '30px',
+  },
+  input: {
+    marginTop: '30px',
+    marginBottom: '10px'
+  },
+  button: {
+    marginTop: '30px',
+    marginBottom: '30px',
+    width: '200px',
+    height: '45px',
+    fontSize: '18px',
+  }
+}))
+
+
+function Register() {
+  const classes = useStyles()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log("form submited");
+  }
+
+  return (
+    <div style={{overflowX: 'hidden'}}>
+      <Grid container className={classes.gridStyle} justify="center" alignItems="center">
+        <Grid item sm={4}>
+          <Card>
+            <CardContent>
+              <Typography variant="h3" className={classes.signup} align="center">
+                Sign-In
+              </Typography>
+              <form onSubmit={handleSubmit}>
+                <div style={{margin: '0px 40px'}}>
+                  <TextField
+                    required
+                    label="Email"
+                    type="email"
+                    placeholder="user@email.com"
+                    variant="outlined"
+                    fullWidth
+                    className={classes.input}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                  <TextField
+                    required
+                    type="password"
+                    label="Password"
+                    placeholder="********"
+                    variant="outlined"
+                    fullWidth
+                    className={classes.input}
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                  <Grid container justify="center" alignItems="center">
+                    <Button variant="contained" color="primary" className={classes.button} type="submit">
+                      Sign-In
+                    </Button>
+                  </Grid>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+          <Typography style={{marginTop: '10px'}}>
+            Not registered? Sign-up
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Register;
